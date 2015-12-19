2b58839a121a2ead3ab730492a58e3f2a78fa899
johannburkard <johann@johannburkard.de>
2015-12-19 20:00:13 +0100

    Unit tested, wee!

---
+++ src/test/groovy/com/eaio/web/ProxyTest.groovy
@@ -0,0 +1,41 @@
+package com.eaio.web
+
+import static org.hamcrest.MatcherAssert.*
+import static org.hamcrest.Matchers.*
+import junitparams.JUnitParamsRunner
+import junitparams.Parameters
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * @author <a href="mailto:johann@johannburkard.de">Johann Burkard</a>
+ * @version $Id$
+ */
+@RunWith(JUnitParamsRunner)
+class ProxyTest {
+    
+    @Lazy
+    Proxy proxy
+
+    /**
+     * Test method for {@link com.eaio.web.Proxy#buildRequestURI(java.lang.String, java.lang.String, java.lang.String)}.
+     */
+    @Test
+    @Parameters(method = 'parameters')
+    void 'buildRequestURI should match expectation'(String scheme, String uri, String queryString, URI expectation) {
+        assertThat(proxy.buildRequestURI(scheme, uri, queryString), is(expectation))
+    }
+    
+    Collection<Object[]> parameters() {
+        [
+            [ 'http', '/http/www.google-analytics.com/ga.js', null, 'http://www.google-analytics.com/ga.js'.toURI() ],
+            [ 'http', '/http/www.google-analytics.com/ga.js', '', 'http://www.google-analytics.com/ga.js'.toURI() ],
+            [ 'http', '/http/www.google-analytics.com/ga.js', 'pruh=guh', 'http://www.google-analytics.com/ga.js?pruh=guh'.toURI() ],
+            [ 'http', '/http/www.google-analytics.com', null, 'http://www.google-analytics.com/'.toURI() ],
+            [ 'https', '/https/www.google-analytics.com/bla/ga.js', null, 'https://www.google-analytics.com/bla/ga.js'.toURI() ],
+        ].collect { it as Object[] }
+    }
+    
+
+}
