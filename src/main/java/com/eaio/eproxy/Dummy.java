373d4f33d753e53ae6531bf67ad6ee8afc4a73ec
johannburkard <johann@johannburkard.de>
2015-12-23 14:42:03 +0100

    Banana.

---
+++ src/main/java/com/eaio/eproxy/Dummy.java
@@ -0,0 +1,13 @@
+package com.eaio.eproxy;
+
+/**
+ * Maven also needs a source file in src/main/java. :(
+ * 
+ * @author <a href="mailto:johann@johannburkard.de">Johann Burkard</a>
+ * @version $Id$
+ */
+final class Dummy {
+
+    private Dummy() {}
+
+}
--- src/test/java/com/eaio/eproxy/DummyTest.java
+++ src/test/java/com/eaio/eproxy/DummyTest.java
@@ -3,7 +3,7 @@ package com.eaio.eproxy;
 import org.junit.Test;
 
 /**
- * Doesn't test anything. Maven's surefire plugin will skip unit tests if there are no classes in src/test/java.
+ * Doesn't test anything. Maven's surefire plugin will skip unit tests if there are no source files in src/test/java.
  * 
  * @author <a href="mailto:johann@johannburkard.de">Johann Burkard</a>
  * @version $Id$
