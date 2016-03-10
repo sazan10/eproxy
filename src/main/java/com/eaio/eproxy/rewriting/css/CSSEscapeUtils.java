89689d754dd128f32aad5363a35cb4a1d6fbbab1
johannburkard <johann@johannburkard.de>
2016-03-10 22:25:17 +0100

    Removing unused code.

--- src/main/java/com/eaio/eproxy/rewriting/css/CSSEscapeUtils.java
+++ src/main/java/com/eaio/eproxy/rewriting/css/CSSEscapeUtils.java
@@ -2,7 +2,6 @@ package com.eaio.eproxy.rewriting.css;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.text.translate.CharSequenceTranslator;
-import org.apache.commons.lang3.text.translate.EntityArrays;
 import org.apache.commons.lang3.text.translate.LookupTranslator;
 
 /**
@@ -21,20 +20,11 @@ public class CSSEscapeUtils {
         { "&rpar;", ")" }
     };
     
-    private static final String[][] CSS_ESCAPE = EntityArrays.invert(CSS_UNESCAPE);
-    
-    public static final CharSequenceTranslator ESCAPE_CSS =
-            new LookupTranslator(CSS_ESCAPE);
-    
     public static final CharSequenceTranslator UNESCAPE_CSS = 
             new LookupTranslator(CSS_UNESCAPE);
 
     public static final String unescapeCSS(final String input) {
         return UNESCAPE_CSS.translate(input);
-    }
-    
-    public static final String escapeCSS(final String input) {
-        return ESCAPE_CSS.translate(input);
     }
 
 }
