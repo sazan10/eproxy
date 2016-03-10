d68f80a6174e9ff6fa766371709fe4fe69908820
Johann Burkard <johann@johannburkard.de>
2016-03-10 18:41:52 +0100

    Exclude data: URIs.

--- src/main/webapp/resources/js-in/telemetry/90-resources-on-different-domains.js
+++ src/main/webapp/resources/js-in/telemetry/90-resources-on-different-domains.js
@@ -19,7 +19,7 @@
 			i, r0, currentURL = decodeTargetURI(location.href)
 		for (i = 0; i < resourceEntries.length; ++i) {
 			r0 = resourceEntries[i]
-			if (!r0.name.startsWith(location.origin) && !r0.name.startsWith('https://www.google-analytics.com/collect')) {
+			if (!/^data:/i.test(r0.name) && !r0.name.startsWith(location.origin) && !r0.name.startsWith('https://www.google-analytics.com/collect')) {
 				eaio.track.event('ResourceOnDifferentDomains', currentURL, decodeTargetURI(r0.name))
 			}
 		}
