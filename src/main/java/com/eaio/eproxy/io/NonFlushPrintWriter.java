67a5020dedb7adb33ec673923ebb8f799732543f
johannburkard <johann@johannburkard.de>
2016-12-11 18:14:46 +0100

    Coverage, I...

--- src/main/java/com/eaio/eproxy/io/NonFlushPrintWriter.java
+++ src/main/java/com/eaio/eproxy/io/NonFlushPrintWriter.java
@@ -21,67 +21,6 @@ public class NonFlushPrintWriter extends PrintWriter {
     }
 
     /**
-     * @param out
-     */
-    public NonFlushPrintWriter(OutputStream out) {
-        super(out);
-    }
-
-    /**
-     * @param fileName
-     * @throws FileNotFoundException
-     */
-    public NonFlushPrintWriter(String fileName) throws FileNotFoundException {
-        super(fileName);
-    }
-
-    /**
-     * @param file
-     * @throws FileNotFoundException
-     */
-    public NonFlushPrintWriter(File file) throws FileNotFoundException {
-        super(file);
-    }
-
-    /**
-     * @param out
-     * @param autoFlush
-     */
-    public NonFlushPrintWriter(Writer out, boolean autoFlush) {
-        super(out, autoFlush);
-    }
-
-    /**
-     * @param out
-     * @param autoFlush
-     */
-    public NonFlushPrintWriter(OutputStream out, boolean autoFlush) {
-        super(out, autoFlush);
-    }
-
-    /**
-     * @param fileName
-     * @param csn
-     * @throws FileNotFoundException
-     * @throws UnsupportedEncodingException
-     */
-    public NonFlushPrintWriter(String fileName, String csn)
-                    throws FileNotFoundException, UnsupportedEncodingException {
-        super(fileName, csn);
-    }
-
-    /**
-     * @param file
-     * @param csn
-     * @throws FileNotFoundException
-     * @throws UnsupportedEncodingException
-     */
-    public NonFlushPrintWriter(File file, String csn)
-                    throws FileNotFoundException, UnsupportedEncodingException {
-        super(file, csn);
-    }
-
-    /**
      * No-op.
      * @see java.io.PrintWriter#flush()
      */
